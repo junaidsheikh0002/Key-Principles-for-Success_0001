0453cf5334838c05af6b94e42a2faaa08eeb54f5
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-10-26 09:03:36 +0500

    https://github.com/junaidsheikh0002/Key-Principles-for-Success_0001/upload
    https://github.com/junaidsheikh0002/Key-Principles-for-Success_0001/upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script is currently empty but can be used for future interactivity
+console.log('Page loaded successfully');
+
+// Example: Displaying a welcome alert when the page loads
+window.onload = () => {
+    alert('Welcome to the Key Principles for Success!');
+};
